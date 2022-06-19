7481a977f70852c1c89287a98add74a39bb6fecf
jorge <jwoliveira18@gmail.com>
2022-06-19 20:38:01 -0300

    Comment component

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -9,7 +9,7 @@ const initialState = [
     user: 1,
     description: "A post description",
     comments: [
-      { comment_user: "livia", comment: "what the fuck is wrong with you" },
+      { comment_user: 2, content: "Comment from user Jorge on post id 1" },
     ],
   },
   {
@@ -19,7 +19,9 @@ const initialState = [
     likedBy: [2, 3, 4, 5],
     user: 1,
     description: "A post description",
-    comments: [{ comment_user: "barbara", comment: "another one" }],
+    comments: [
+      { comment_user: 4, content: "Comment from user Barb on post id 2" },
+    ],
   },
   {
     id: 3,
@@ -28,7 +30,9 @@ const initialState = [
     likedBy: [1, 3, 4, 5],
     user: 2,
     description: "A post description",
-    comments: [{ comment_user: "ivanilza", comment: "lavar os prato" }],
+    comments: [
+      { comment_user: 4, content: "Comment from user Barb on post id 3" },
+    ],
   },
   {
     id: 4,
@@ -37,7 +41,9 @@ const initialState = [
     likedBy: [1, 3, 4, 5],
     user: 2,
     description: "A post description",
-    comments: [{ comment_user: "ivanilza", comment: "lavar os prato" }],
+    comments: [
+      { comment_user: 3, content: "Comment from user Livia on post id 4" },
+    ],
   },
   {
     id: 5,
@@ -46,7 +52,9 @@ const initialState = [
     likedBy: [1, 2, 4, 5],
     user: 3,
     description: "A post description",
-    comments: [{ comment_user: "ivanilza", comment: "lavar os prato" }],
+    comments: [
+      { comment_user: 1, content: "Comment from user Gabi on post id 5" },
+    ],
   },
   {
     id: 6,
@@ -55,7 +63,9 @@ const initialState = [
     likedBy: [1, 2, 4, 5],
     user: 3,
     description: "A post description",
-    comments: [{ comment_user: "ivanilza", comment: "lavar os prato" }],
+    comments: [
+      { comment_user: 5, content: "Comment from user Dudu on post id 6" },
+    ],
   },
   {
     id: 7,
@@ -64,7 +74,9 @@ const initialState = [
     likedBy: [1, 2, 3, 5],
     user: 4,
     description: "A post description",
-    comments: [{ comment_user: "ivanilza", comment: "lavar os prato" }],
+    comments: [
+      { comment_user: 5, content: "Comment from user Dudu on post id 7" },
+    ],
   },
   {
     id: 8,
@@ -73,7 +85,9 @@ const initialState = [
     likedBy: [1, 2, 3, 5],
     user: 4,
     description: "A post description",
-    comments: [{ comment_user: "ivanilza", comment: "lavar os prato" }],
+    comments: [
+      { comment_user: 3, content: "Comment from user Livia on post id 8" },
+    ],
   },
   {
     id: 9,
@@ -82,7 +96,9 @@ const initialState = [
     likedBy: [1, 2, 3, 4],
     user: 5,
     description: "A post description",
-    comments: [{ comment_user: "ivanilza", comment: "lavar os prato" }],
+    comments: [
+      { comment_user: 2, content: "Comment from user Jorge on post id 9" },
+    ],
   },
   {
     id: 10,
@@ -91,7 +107,9 @@ const initialState = [
     likedBy: [1, 2, 3, 4],
     user: 5,
     description: "A post description",
-    comments: [{ comment_user: "ivanilza", comment: "lavar os prato" }],
+    comments: [
+      { comment_user: 1, content: "Comment from user Gabi on post id 10" },
+    ],
   },
 ];
 
